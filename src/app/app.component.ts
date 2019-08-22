a6dfed5b2f9b8c5386e0008fe7e326879042642a
Tim Wright <dtime35@gmail.com>
2019-08-21 18:32:54 -0600

    init project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'umbra-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'umbra';
+}
